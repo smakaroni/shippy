38554e0ce4ac5665c1074f5d30defe309a3f3a27
Jokke <smakihani@gmail.com>
2017-12-27 14:03:51 +0100

    First commit

---
+++ consignment-service/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	pb "github.com/smakaroni/shippy/consignment-service/proto/consignment"
+	"golang.org/x/net/context"
+	micro "github.com/micro/go-micro"
+	"fmt"
+)
+
+const (
+	port = ":50051"
+)
+
+type IRepository interface{
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+type Repository struct {
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo IRepository
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	//Save consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+
+	res.Created = true
+	res.Consignment = consignment
+	return nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
+}
+
+func main() {
+
+	repo := &Repository{}
+
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.consignment"),
+		micro.Version("latest"),
+	)
+
+	srv.Init()
+
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
